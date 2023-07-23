c4a0b80ed3dcfe808f1ab2684c3f614d28ec5975
Aritro Saha <29025984+AritroSaha10@users.noreply.github.com>
2023-07-23 15:31:24 -0400

    feat: add majority of event model

--- backend/models/event.go
+++ backend/models/event.go
@@ -1,9 +1,11 @@
 package models
 
 import (
+	"context"
 	"net/http"
 	"time"
 
+	"github.com/aritrosaha10/frasertickets/lib"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
@@ -21,4 +23,84 @@ type Event struct {
 
 func (event *Event) Render(w http.ResponseWriter, r *http.Request) error {
 	return nil
+}
+
+func GetAllEvents(ctx context.Context) ([]Event, error) {
+	// Try to get data from MongoDB
+	cursor, err := lib.Datastore.Db.Collection(eventsColName).Find(ctx, bson.D{})
+	if err != nil {
+		return []Event{}, err
+	}
+
+	// Attempt to convert BSON data into Event structs
+	var events []Event
+	if err := cursor.All(ctx, &events); err != nil {
+		return []Event{}, err
+	}
+
+	return events, nil
+}
+
+func GetEventByKey(ctx context.Context, key string, value string) (Event, error) {
+	// Try to fetch data from DB
+	var event Event
+	err := lib.Datastore.Db.Collection(eventsColName).FindOne(ctx, bson.D{{Key: key, Value: value}}).Decode(&event)
+
+	// No error handling needed (user & err will default to empty struct / nil)
+	return event, err
+}
+
+func CreateNewEvent(ctx context.Context, event Event) (string, error) {
+	// Try to add document
+	res, err := lib.Datastore.Db.Collection(eventsColName).InsertOne(ctx, event)
+
+	// Return object ID
+	return res.InsertedID.(string), err
+}
+
+func UpdateExistingEvent(ctx context.Context, id string, updates map[string]interface{}) error {
+	UPDATABLE_KEYS := map[string]bool{
+		"name":            true,
+		"description":     true,
+		"img_url":         true,
+		"location":        true,
+		"start_timestamp": true,
+		"end_timestamp":   true,
+	}
+
+	// Convert the string/interface map to BSON updates
+	bsonUpdates := bson.D{}
+	for key, val := range updates {
+		// Don't allow other keys to be updated
+		if !UPDATABLE_KEYS[key] {
+			return ErrEditNotAllowed
+		}
+
+		// Add the key/val pair in BSON
+		bsonUpdates = append(bsonUpdates, bson.E{Key: key, Value: val})
+	}
+
+	// Try to update document in DB
+	res, err := lib.Datastore.Db.Collection(eventsColName).UpdateByID(ctx, id, bson.D{{Key: "$set", Value: bsonUpdates}})
+	if err != nil {
+		return err
+	}
+	if res.ModifiedCount == 0 {
+		return ErrNoDocumentModified
+	}
+	return nil
+}
+
+func DeleteEvent(ctx context.Context, id string) error {
+	// Check if event exists
+	_, err := GetEventByKey(ctx, "_id", id)
+	if err != nil {
+		return err
+	}
+
+	// TODO: Delete all tickets to event
+
+	// Delete event
+	_, err = lib.Datastore.Db.Collection(eventsColName).DeleteOne(ctx, bson.E{Key: "_id", Value: id})
+	return err
 }
