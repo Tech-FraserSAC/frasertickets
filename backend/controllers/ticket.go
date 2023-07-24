9410f5364a0cbf3b3c722d1edf22eed69d7ba7f5
Aritro Saha <29025984+AritroSaha10@users.noreply.github.com>
2023-07-24 16:34:37 -0400

    feat(backend): complete impls for ticket crtlr

--- backend/controllers/ticket.go
+++ backend/controllers/ticket.go
@@ -1,19 +1,40 @@
 package controllers
 
 import (
+	"encoding/json"
 	"net/http"
+	"time"
 
+	"github.com/aritrosaha10/frasertickets/models"
+	"github.com/aritrosaha10/frasertickets/util"
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/render"
+	"github.com/go-playground/validator/v10"
+	"github.com/rs/zerolog/log"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
 )
+
+type ticketControllerCreateRequestBody struct {
+	OwnerID string `json:"ownerID" validate:"required,mongodb"`
+	EventID string `json:"eventID" validate:"required,mongodb"`
+}
+
+type ticketControllerSearchRequestBody struct {
+	OwnerID string `json:"ownerID" validate:"required,mongodb"`
+	EventID string `json:"eventID" validate:"required,mongodb"`
+}
 
 type TicketController struct{}
 
 func (ctrl TicketController) Routes() chi.Router {
 	r := chi.NewRouter()
 
-	r.Get("/", ctrl.ListSelf)   // GET /tickets - returns the requester's tickets, available to any user
-	r.Get("/all", ctrl.ListAll) // GET /tickets/all - returns all tickets, only available to admins
-	r.Post("/", ctrl.Create)    // POST /tickets - create a new ticket, only available to admins
+	r.Get("/", ctrl.ListSelf)      // GET /tickets - returns the requester's tickets, available to any user
+	r.Post("/", ctrl.Create)       // POST /tickets - create a new ticket, only available to admins
+	r.Get("/all", ctrl.ListAll)    // GET /tickets/all - returns all tickets, only available to admins
+	r.Post("/search", ctrl.Search) // POST /tickets/search - search for a ticket given an owner and event, only available to admins
 
 	r.Route("/user/{uid}", func(r chi.Router) {
 		r.Get("/", ctrl.ListUser) // GET /tickets/user/{uid} - returns a user's tickets, only available to admins
@@ -28,25 +49,234 @@ func (ctrl TicketController) Routes() chi.Router {
 }
 
 func (ctrl TicketController) ListSelf(w http.ResponseWriter, r *http.Request) {
-
+	// TODO: Work on this, can only do after auth object passed into context
 }
 
 func (ctrl TicketController) ListUser(w http.ResponseWriter, r *http.Request) {
+	// Get user UID
+	uid := chi.URLParam(r, "uid")
 
+	// Check if user exists
+	exists, err := models.CheckIfUserExists(r.Context(), uid)
+	if err != nil {
+		log.Error().Err(err).Msg("could not check if user exists")
+		render.Render(w, r, util.ErrServer(err))
+		return
+	} else if !exists {
+		log.Info().Err(err).Str("uid", uid).Msg("user could not be found")
+		render.Render(w, r, util.ErrNotFound)
+		return
+	}
+
+	// Try to get tickets
+	tickets, err := models.GetTickets(r.Context(), bson.M{"owner": uid})
+	if err != nil {
+		log.Error().Err(err).Str("uid", uid).Msg("could not fetch user's tickets")
+		render.Render(w, r, util.ErrServer(err))
+		return
+	}
+
+	// Convert into list of renderers to turn into JSON
+	renderers := []render.Renderer{}
+	for _, ticket := range tickets {
+		t := ticket // Duplicate it before passing by reference to avoid only passing the last user obj
+		renderers = append(renderers, &t)
+	}
+
+	// Return as JSON array, fallback if it fails
+	if err := render.RenderList(w, r, renderers); err != nil {
+		render.Render(w, r, util.ErrRender(err))
+		return
+	}
 }
 
 func (ctrl TicketController) ListAll(w http.ResponseWriter, r *http.Request) {
+	// Try to get tickets
+	tickets, err := models.GetTickets(r.Context(), bson.M{})
+	if err != nil {
+		log.Error().Err(err).Msg("could not fetch all tickets")
+		render.Render(w, r, util.ErrServer(err))
+		return
+	}
 
+	// Convert into list of renderers to turn into JSON
+	renderers := []render.Renderer{}
+	for _, ticket := range tickets {
+		t := ticket // Duplicate it before passing by reference to avoid only passing the last user obj
+		renderers = append(renderers, &t)
+	}
+
+	// Return as JSON array, fallback if it fails
+	if err := render.RenderList(w, r, renderers); err != nil {
+		render.Render(w, r, util.ErrRender(err))
+		return
+	}
 }
 
 func (ctrl TicketController) Create(w http.ResponseWriter, r *http.Request) {
+	var (
+		ticketRaw ticketControllerCreateRequestBody
+		ticket    models.Ticket
+	)
+
+	// Parse JSON body
+	bodyDecoder := json.NewDecoder(r.Body)
+	bodyDecoder.DisallowUnknownFields()
+	err := bodyDecoder.Decode(&ticketRaw)
+	if err != nil {
+		log.Error().Err(err).Msg("could not parse body")
+		render.Render(w, r, util.ErrInvalidRequest(err))
+		return
+	}
+
+	// Validate JSON body
+	validate := validator.New()
+	err = validate.Struct(ticketRaw)
+	if err != nil {
+		log.Error().Err(err).Msg("could not validate body")
+		render.Render(w, r, util.ErrInvalidRequest(err))
+		return
+	}
+
+	// Convert to ObjectID from string
+	eventID, err := primitive.ObjectIDFromHex(ticketRaw.EventID)
+	if err != nil {
+		log.Error().Err(err).Str("id", ticketRaw.EventID).Msg("could not parse event id")
+		render.Render(w, r, util.ErrServer(err))
+		return
+	}
+
+	// Transfer all data from raw to actual ticket
+	ticket.Owner = ticketRaw.OwnerID
+	ticket.Event = eventID
+	ticket.Timestamp = time.Now()
+
+	// Try to add to DB
+	id, err := models.CreateNewTicket(r.Context(), ticket)
+	if err != nil {
+		log.Error().Err(err).Msg("could not add ticket to DB")
+		render.Render(w, r, util.ErrServer(err))
+		return
+	}
+	ticket.ID = id
+
+	// Return as JSON, fallback if it fails
+	if err := render.Render(w, r, &ticket); err != nil {
+		render.Render(w, r, util.ErrRender(err))
+		return
+	}
 
 }
 
 func (ctrl TicketController) Get(w http.ResponseWriter, r *http.Request) {
+	// Get ID of requested ticket
+	id := chi.URLParam(r, "id")
 
+	// Try to convert the given ID into an Object ID
+	objID, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		log.Error().Err(err).Msg("could not convert hex to object id")
+		render.Render(w, r, util.ErrInvalidRequest(err))
+		return
+	}
+
+	// Try to fetch from DB
+	ticket, err := models.GetTicket(r.Context(), objID)
+
+	// Handle errors
+	if err != nil {
+		if err == mongo.ErrNoDocuments {
+			render.Render(w, r, util.ErrNotFound)
+			return
+		}
+
+		log.Error().Err(err).Msg("could not find ticket")
+		render.Render(w, r, util.ErrServer(err))
+		return
+	}
+
+	// Return as JSON, fallback if it fails
+	if err := render.Render(w, r, &ticket); err != nil {
+		render.Render(w, r, util.ErrRender(err))
+		return
+	}
+}
+
+func (ctrl TicketController) Search(w http.ResponseWriter, r *http.Request) {
+	var searchQuery ticketControllerSearchRequestBody
+
+	// Parse JSON body
+	bodyDecoder := json.NewDecoder(r.Body)
+	bodyDecoder.DisallowUnknownFields()
+	err := bodyDecoder.Decode(&searchQuery)
+	if err != nil {
+		log.Error().Err(err).Msg("could not parse body")
+		render.Render(w, r, util.ErrInvalidRequest(err))
+		return
+	}
+
+	// Validate JSON body
+	validate := validator.New()
+	err = validate.Struct(searchQuery)
+	if err != nil {
+		log.Error().Err(err).Msg("could not validate body")
+		render.Render(w, r, util.ErrInvalidRequest(err))
+		return
+	}
+
+	// Convert to ObjectID from string
+	eventID, err := primitive.ObjectIDFromHex(searchQuery.EventID)
+	if err != nil {
+		log.Error().Err(err).Str("id", searchQuery.EventID).Msg("could not parse event id")
+		render.Render(w, r, util.ErrServer(err))
+		return
+	}
+
+	// Try to fetch from DB
+	ticket, err := models.SearchForTicket(r.Context(), eventID, searchQuery.OwnerID)
+
+	// Handle errors
+	if err != nil {
+		if err == mongo.ErrNoDocuments {
+			render.Render(w, r, util.ErrNotFound)
+			return
+		}
+
+		log.Error().Err(err).Msg("could not find ticket")
+		render.Render(w, r, util.ErrServer(err))
+		return
+	}
+
+	// Return as JSON, fallback if it fails
+	if err := render.Render(w, r, &ticket); err != nil {
+		render.Render(w, r, util.ErrRender(err))
+		return
+	}
 }
 
 func (ctrl TicketController) Delete(w http.ResponseWriter, r *http.Request) {
+	// Get ID of requested ticket
+	id := chi.URLParam(r, "id")
 
+	// Convert to ObjectID
+	objID, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		log.Error().Err(err).Msg("could not convert id to objectid")
+		render.Render(w, r, util.ErrInvalidRequest(err))
+		return
+	}
+
+	// Try to delete document
+	err = models.DeleteTicket(r.Context(), objID)
+	if err == models.ErrNoDocumentModified || err == mongo.ErrNoDocuments {
+		log.Error().Err(err).Msg("could not find ticket to delete")
+		render.Render(w, r, util.ErrNotFound)
+		return
+	} else if err != nil {
+		log.Error().Err(err).Msg("could not delete ticket")
+		render.Render(w, r, util.ErrServer(err))
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
 }
