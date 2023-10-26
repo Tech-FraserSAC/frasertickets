94881b317b322e41c3d0cbea35570e6696a02cae
Aritro Saha <29025984+AritroSaha10@users.noreply.github.com>
2023-10-26 18:39:38 -0400

    fix(frontend): keep a WIP notice on scanner page

--- frontend/pages/admin/scan.tsx
+++ frontend/pages/admin/scan.tsx
@@ -1,9 +1,10 @@
 import Layout from "@/components/admin/Layout";
+import { Typography } from "@material-tailwind/react";
 
 export default function TicketScanningPage() {
     return (
         <Layout name="Ticket Scan" className="p-4 md:p-8 lg:px-12">
-            
+            <Typography variant="h1" className="text-center">Work in Progress!</Typography>
         </Layout>
     )
 }
