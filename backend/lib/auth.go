e0a880e86baddf62a51437fd687b7ad442fe191d
Aritro Saha <29025984+AritroSaha10@users.noreply.github.com>
2023-10-26 20:12:43 -0400

    fix(backend): use ADC for gcloud if in prod env

--- backend/lib/auth.go
+++ backend/lib/auth.go
@@ -25,25 +25,27 @@ type IdentityPlatformAuth struct {
 func CreateNewAuth() *IdentityPlatformAuth {
 	auth := &IdentityPlatformAuth{}
 
-	// Import and format credentials from env to JSON
-	firebaseCreds := map[string]string{
-		"type":                        "service_account",
-		"project_id":                  os.Getenv("FIREBASE_PROJECT_ID"),
-		"private_key_id":              os.Getenv("FIREBASE_PRIVATE_KEY_ID"),
-		"private_key":                 os.Getenv("FIREBASE_PRIVATE_KEY"),
-		"client_email":                os.Getenv("FIREBASE_CLIENT_EMAIL"),
-		"client_id":                   os.Getenv("FIREBASE_CLIENT_ID"),
-		"auth_uri":                    "https://accounts.google.com/o/oauth2/auth",
-		"token_uri":                   "https://oauth2.googleapis.com/token",
-		"auth_provider_x509_cert_url": os.Getenv("FIREBASE_AUTH_PROVIDER_CERT_URL"),
-		"client_x509_cert_url":        os.Getenv("FIREBASE_CLIENT_CERT_URL"),
-		"universe_domain":             "googleapis.com",
+	if os.Getenv("FRASERTICKETS_ENV") == "development" {
+		// Import and format credentials from env to JSON
+		firebaseCreds := map[string]string{
+			"type":                        "service_account",
+			"project_id":                  os.Getenv("FIREBASE_PROJECT_ID"),
+			"private_key_id":              os.Getenv("FIREBASE_PRIVATE_KEY_ID"),
+			"private_key":                 os.Getenv("FIREBASE_PRIVATE_KEY"),
+			"client_email":                os.Getenv("FIREBASE_CLIENT_EMAIL"),
+			"client_id":                   os.Getenv("FIREBASE_CLIENT_ID"),
+			"auth_uri":                    "https://accounts.google.com/o/oauth2/auth",
+			"token_uri":                   "https://oauth2.googleapis.com/token",
+			"auth_provider_x509_cert_url": os.Getenv("FIREBASE_AUTH_PROVIDER_CERT_URL"),
+			"client_x509_cert_url":        os.Getenv("FIREBASE_CLIENT_CERT_URL"),
+			"universe_domain":             "googleapis.com",
+		}
+		serviceAccountJSON, err := json.Marshal(firebaseCreds)
+		if err != nil {
+			log.Fatal().Err(err).Msg("could not marshal firebase creds from env")
+		}
+		auth.credentials = option.WithCredentialsJSON(serviceAccountJSON)
 	}
-	serviceAccountJSON, err := json.Marshal(firebaseCreds)
-	if err != nil {
-		log.Fatal().Err(err).Msg("could not marshal firebase creds from env")
-	}
-	auth.credentials = option.WithCredentialsJSON(serviceAccountJSON)
 
 	// Initialize Firebase app
 	fbApp, err := firebase.NewApp(context.Background(), nil, auth.credentials)
