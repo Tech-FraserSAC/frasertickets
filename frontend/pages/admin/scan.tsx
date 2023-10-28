ff73bdb3a58a0c1010fc786e96686affeb14443d
Aritro Saha <29025984+AritroSaha10@users.noreply.github.com>
2023-10-28 00:37:45 -0400

    feat(admin/frontend): prelim version of ticket scanner

--- frontend/pages/admin/scan.tsx
+++ frontend/pages/admin/scan.tsx
@@ -1,10 +1,29 @@
 import Layout from "@/components/admin/Layout";
 import { Typography } from "@material-tailwind/react";
+import ZXingBrowser, { BrowserQRCodeReader } from '@zxing/browser';
+import { useEffect, useRef } from "react";
 
 export default function TicketScanningPage() {
+    const codeReader = new BrowserQRCodeReader()
+    const previewElem = useRef<HTMLVideoElement | null>(null);
+
+    useEffect(() => {
+        (async () => {
+            const videoInputDevices = await ZXingBrowser.BrowserCodeReader.listVideoInputDevices()
+            const selectedDeviceId = videoInputDevices[0].deviceId;
+            console.log(selectedDeviceId)
+            const controls = await codeReader.decodeFromVideoDevice(selectedDeviceId, previewElem.current === null ? undefined : previewElem.current, (res, err, controls) => {
+                console.log(res, err)
+                controls.stop()
+            })
+        })()
+    }, [])
+
     return (
         <Layout name="Ticket Scan" className="p-4 md:p-8 lg:px-12">
             <Typography variant="h1" className="text-center">Work in Progress!</Typography>
+
+            <video ref={previewElem} />
         </Layout>
     )
 }
