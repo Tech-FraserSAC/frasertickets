60ada983eb9be725dcd964a2c9d58c5a1275dfef
Aritro Saha <29025984+AritroSaha10@users.noreply.github.com>
2023-10-29 23:52:44 -0400

    fix(frontend): upgrade to next14

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,7 +2,16 @@
 const nextConfig = {
   reactStrictMode: false,
   images: {
-    domains: ['storage.googleapis.com', 'lh3.googleusercontent.com']
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'storage.googleapis.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      }
+    ]
   },
   async rewrites() {
     return [
