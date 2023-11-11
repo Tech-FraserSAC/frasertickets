a7cfca29131ac26be16434490074f4379230c044
Aritro Saha <29025984+AritroSaha10@users.noreply.github.com>
2023-11-10 21:11:14 -0500

    fix(frontend): improve freshness policy for tokens on admin routes

--- frontend/lib/backend/sendBackendRequest.ts
+++ frontend/lib/backend/sendBackendRequest.ts
@@ -11,25 +11,34 @@ export default async function sendBackendRequest(path: string, method: Method, a
     // Get token if authentication required
     let bearerToken = ""
     if (authenticate === undefined || authenticate === true) {
-        console.time("firebase auth ready")
+        const log_id = Math.round(Math.random() * 100000)
+        console.time(`auth ready (req id ${log_id})`)
         await auth.authStateReady()
-        console.timeEnd("firebase auth ready")
+        console.timeEnd(`auth ready (req id ${log_id})`)
         
         const user = auth.currentUser
         if (user === null) {
             throw "User is not signed in"
         }
 
-        // Always make sure to generate a new token for admin routes,
+        // Always make sure to use very fresh tokens for admin routes,
         // since their admin status is stored in the token itself
         // and should always be up-to-date. An example case where using 
         // the cached token could go wrong is if a user has been given 
         // or had their admin privileges revoked. In this case, the user would
         // still have the same permissions as they did before, until the token
-        // expires (max. 5 min).
-        console.time("firebase token ready")
-        const token = await user.getIdToken(!!adminRoute)
-        console.timeEnd("firebase token ready")
+        // expires (max. 1 hour).
+        let token: string
+        console.time(`token ready (req id ${log_id})`)
+        // Refresh token for admin routes if more than 5 minutes old
+        const tokenDecoded = await user.getIdTokenResult()
+        const tokenAge = Date.now() - new Date(tokenDecoded.issuedAtTime).getTime();
+        if (!!adminRoute && tokenAge > 5 * 60 * 1000) {
+            token = await user.getIdToken(true)
+        } else {
+            token = tokenDecoded.token
+        }
+        console.timeEnd(`token ready (req id ${log_id})`)
 
         bearerToken = `Bearer ${token}`
     }
