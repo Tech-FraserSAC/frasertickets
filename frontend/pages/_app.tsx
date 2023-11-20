dd870cd5adede2bd47e3859be72dd8b5c6c38fec
Aritro Saha <29025984+AritroSaha10@users.noreply.github.com>
2023-11-20 14:16:01 -0500

    fix(frontend): optimize app.tsx by lazy loading providers

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,13 +1,17 @@
-import { FirebaseAuthProvider } from '@/components/FirebaseAuthContext'
-import '@/styles/globals.css'
-import { ThemeProvider } from '@material-tailwind/react'
-import { AnimatePresence, LazyMotion, domAnimation } from 'framer-motion'
-import type { AppProps } from 'next/app'
-import { QueryClient, QueryClientProvider } from 'react-query'
-import { GoogleAnalytics } from "nextjs-google-analytics";
 import dynamic from 'next/dynamic'
-import { Montserrat, Poppins } from 'next/font/google'
-import { GoogleOAuthProvider } from '@react-oauth/google';
+
+const FirebaseAuthProvider = dynamic(() => import("@/components/FirebaseAuthContext").then(mod => mod.FirebaseAuthProvider))
+const QueryClientProvider = dynamic(() => import("react-query").then(mod => mod.QueryClientProvider))
+const ThemeProvider = dynamic(() => import("@material-tailwind/react").then(mod => mod.ThemeProvider))
+const AnimatePresence = dynamic(() => import("framer-motion").then(mod => mod.AnimatePresence))
+const LazyMotion = dynamic(() => import("framer-motion").then(mod => mod.LazyMotion))
+const GoogleAnalytics = dynamic(() => import("nextjs-google-analytics").then(mod => mod.GoogleAnalytics))
+const GoogleOAuthProvider = dynamic(() => import("@react-oauth/google").then(mod => mod.GoogleOAuthProvider))
+
+import { QueryClient } from 'react-query'
+import { Poppins } from 'next/font/google'
+import type { AppProps } from 'next/app'
+import '@/styles/globals.css'
 
 const domMax = () => import("@/lib/anim/domMax").then(res => res.default)
 
