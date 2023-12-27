f7fe7614fb10254f5cb4d1b5896069c293f3d0ac
Aritro Saha <29025984+AritroSaha10@users.noreply.github.com>
2023-12-27 15:49:30 -0500

    fix(backend): enable sentry profiling (closes #73)

--- backend/middleware/sentry.go
+++ backend/middleware/sentry.go
@@ -11,16 +11,15 @@ import (
 func CreateNewSentryMiddleware() *sentryhttp.Handler {
 	// Init Sentry first
 	if err := sentry.Init(sentry.ClientOptions{
-		Dsn:           "https://1e36660a7a5e022a8f602034da95eca7@o4506142641356800.ingest.sentry.io/4506142665277440",
-		EnableTracing: true,
-		// Set TracesSampleRate to 1.0 to capture 100%
-		// of transactions for performance monitoring.
-		// We recommend adjusting this value in production,
-		TracesSampleRate: 1.0,
+		Dsn:                "https://1e36660a7a5e022a8f602034da95eca7@o4506142641356800.ingest.sentry.io/4506142665277440",
+		EnableTracing:      true,
+		TracesSampleRate:   1.0,
+		ProfilesSampleRate: 1.0,
 	}); err != nil {
 		log.Error().Err(err).Msg("Sentry initialization failed")
 	}
 	defer sentry.Flush(time.Second)
+	log.Debug().Msg("Sentry initialized")
 
 	return sentryhttp.New(sentryhttp.Options{
 		Repanic: true,
