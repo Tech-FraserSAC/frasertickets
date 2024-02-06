2f34b29222f7c5ed735a2c4592c6a48a75bfbce6
Aritro Saha <29025984+AritroSaha10@users.noreply.github.com>
2024-02-06 09:13:08 -0500

    experiment w/ gwallet integration

--- backend/go.mod
+++ backend/go.mod
@@ -33,6 +33,7 @@ require (
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.14.1 // indirect
+	github.com/golang-jwt/jwt v3.2.2+incompatible
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/golang/snappy v0.0.4 // indirect
--- backend/util/gcp.go
+++ backend/util/gcp.go
@@ -5,6 +5,8 @@ import (
 	"errors"
 	"os"
 
+	"golang.org/x/oauth2/google"
+	"golang.org/x/oauth2/jwt"
 	"google.golang.org/api/option"
 )
 
@@ -29,6 +31,36 @@ func PrepareGCPCredentialsFromEnv() (option.ClientOption, error) {
 			return nil, err
 		}
 		return option.WithCredentialsJSON(serviceAccountJSON), nil
+	} else {
+		return nil, errors.New("could not find FIREBASE_PROJECT_ID in env")
+	}
+}
+
+func PrepareJWTConfigFromEnv(scope string) (*jwt.Config, error) {
+	if _, present := os.LookupEnv("FIREBASE_PROJECT_ID"); present {
+		// Import and format credentials from env to JSON
+		firebaseCreds := map[string]string{
+			"type":                        "service_account",
+			"project_id":                  os.Getenv("FIREBASE_PROJECT_ID"),
+			"private_key_id":              os.Getenv("FIREBASE_PRIVATE_KEY_ID"),
+			"private_key":                 os.Getenv("FIREBASE_PRIVATE_KEY"),
+			"client_email":                os.Getenv("FIREBASE_CLIENT_EMAIL"),
+			"client_id":                   os.Getenv("FIREBASE_CLIENT_ID"),
+			"auth_uri":                    "https://accounts.google.com/o/oauth2/auth",
+			"token_uri":                   "https://oauth2.googleapis.com/token",
+			"auth_provider_x509_cert_url": os.Getenv("FIREBASE_AUTH_PROVIDER_CERT_URL"),
+			"client_x509_cert_url":        os.Getenv("FIREBASE_CLIENT_CERT_URL"),
+			"universe_domain":             "googleapis.com",
+		}
+		serviceAccountJSON, err := json.Marshal(firebaseCreds)
+		if err != nil {
+			return nil, err
+		}
+		jwtConfig, err := google.JWTConfigFromJSON(serviceAccountJSON, scope)
+		if err != nil {
+			return nil, err
+		}
+		return jwtConfig, nil
 	} else {
 		return nil, errors.New("could not find FIREBASE_PROJECT_ID in env")
 	}
